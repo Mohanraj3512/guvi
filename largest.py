3639638cd72175955f7541ab9f8204058ccfaf20
Mohanraj3512 <50900631+Mohanraj3512@users.noreply.github.com>
2019-05-23 15:42:40 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,9 @@
+a=int(input("enter the A value  "))
+b=int(input("enter the B value  "))
+c=int(input("enter the C value  "))
+if((b<=a)and(c<=a)):
+  print("given input is A is greater ")
+elif((a<=b)and(c<=b)):
+  print("given input is B is greater ")
+else:
+  print("given input is c is greater ")
