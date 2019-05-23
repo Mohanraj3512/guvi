fbb91cc1e1d1987a4a7fd5ab64fb9f08747d7601
Mohanraj3512 <50900631+Mohanraj3512@users.noreply.github.com>
2019-05-23 15:16:42 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+a=input("enter the alphabet  ")
+if((a=="a")or(a=="A")or(a=="e")or(a=="E")or(a=="i")or(a=="I")or(a=="o")or(a=="O")or(a=="u")or(a=="U")):
+  print("given input is vowel ")
+else:
+  print("given input is consonant")
