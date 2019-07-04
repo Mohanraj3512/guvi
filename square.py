4f77192e2d331f8dbfd26588902d783040330963
Mohanraj3512 <50900631+Mohanraj3512@users.noreply.github.com>
2019-07-04 09:44:17 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,12 +1,2 @@
-n=input("")  
-s=n.split()
-a=str(s[0])
-a=int(a)
-b=s[1]
-b=int(b)
-sum=0
-while(b <=1):
-  sum=a
-  sum=sum*a
-  b=b-1
-print("The sum is",sum)
+a,b=map(int,input().split())
+print(a ** b)
